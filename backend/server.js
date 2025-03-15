1e4e7bd8f0f08ae844829169fe422f0322960b32
Indranil Mondal <indranil12.cricket@gmail.com>
2025-03-15 21:59:26 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect("mongodb://localhost:27017/ruleEngine", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
